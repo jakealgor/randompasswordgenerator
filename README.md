5fa702ee6f8e0098c1580de6de7d200d596bb14d
jakealgor <jake.algor@gmail.com>
2020-12-07 19:50:27 -0500

    new repo, old onw had too many issues, changed generater function, major style changes, added select fields

---
+++ README.md
@@ -0,0 +1,27 @@
+#### Password Generator
+
+### Welcome to Password Generator
+
+* Use this site to create a new random password! 
+
+## How to use?
+
+# Step One: Slider
+
+* Use the slider shown to set an amount of characters
+
+# Step two: Character Quanity
+
+* View how many characters you would like. Once you have found a number of characters you would like, leave the slider in place.
+
+# Step three: Set character types
+* Pick which character fields you would like to include into your password.
+
+# Step four: Create
+
+* Hit create! Your generated password will be shown at the top.
+
+# Step five: Copy
+
+* If you like the password that has been generated, hit "Copy to Clipboard"
+
---
+++ javascript.js
@@ -0,0 +1,107 @@
+const resultEl = document.getElementById("result");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const numberEl = document.getElementById("number");
+const symbolEl = document.getElementById("symbol");
+const generateEl = document.getElementById('generate');
+const copyEl = document.getElementById("copy");
+
+const gen = {
+    lower: getLowerCase,
+    upper: getUpperCase,
+    number: getNumber,
+    symbol: getSymbol,
+};
+
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value;
+    const haslow = lowercaseEl.checked;
+    const hasupper = uppercaseEl.checked;
+    const hasnumber = numberEl.checked;
+    const hassym = symbolEl.checked;
+    resultEl.innerText = generatePassword(haslow, hasupper, hasnumber, hassym, length);
+});
+
+copyEl.addEventListener('click', () => {
+    const textarea = document.createElement('textarea');
+    const password = resultEl.innerText;
+    if(!password) {
+        return;
+    }
+
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+    alert('Password has been Copied!')
+})
+
+function generatePassword(lower, upper, number, symbol, length){
+    let generatedPassword = '';
+    const typesCount = lower + upper + number + symbol;
+    const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0]);
+    if(typesCount === 0) {
+        return '';
+    }
+    for(let i = 0; i < length; i += typesCount){
+        typesArr.forEach(type =>{
+            const funcName = Object.keys(type)[0];
+            generatedPassword += gen[funcName]();
+        })
+    }
+
+    const finalPassword = generatedPassword.slice(0, length);
+    return finalPassword;
+}
+
+function getLowerCase(){
+    const lowercase = "abcdefghijklmnopqrstuvwxyz";
+    return lowercase [Math.floor(Math.random() * lowercase.length)];
+}
+
+function getUpperCase(){
+    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    return uppercase [Math.floor(Math.random() * uppercase.length)];
+}
+
+function getNumber(){
+    const numbs = "1234567890";
+    return numbs [Math.floor(Math.random() * numbs.length)];
+}
+
+function getSymbol(){
+    const symbol = "!?$%^&().'/";
+    return symbol [Math.floor(Math.random() * symbol.length)];
+} 
+
+
+
+// function passwordgenerator(){
+//     let characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWZYZ1234567890!?$%^&().'/";
+//     let password = "";
+//     let generate = document.getElementById("slide").value;
+//     for(const i = 0; i <= generate; i++){
+//         password = password + characters.charAt(Math.floor(Math.random() * Math.floor(characters.length - 1)));
+//     }
+//     document.getElementById("show").value = password;
+//     document.getElementById("count").innerHTML += password + "<br />";
+// }
+
+// function copy(){
+//     document.getElementById("show").select();
+//     document.execCommand("Copy");
+//     alert("Password copied to your clipboard!");
+// }
+
+// document.getElementById("characteramount").innerHTML = "Character Count: 28";
+
+// document.getElementById("slide").oninput = function(){
+//     if(document.getElementById("slide").value > 8){
+//         document.getElementById("characteramount").innerHTML = "Character Count: " + document.getElementById("slide").value;
+//     }
+//     else{
+//         document.getElementById("characteramount").innerHTML = "Character Count: 8";
+//     }
+// }
